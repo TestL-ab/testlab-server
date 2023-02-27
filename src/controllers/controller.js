ca351e8e38aa700a767ff28f8777ed938ee29b4e
Alison Martinez <alison.a.martinez@outlook.com>
2023-02-27 15:29:36 -0500

    Starting added variants

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -3,8 +3,6 @@ import config from "../utils/config.js";
 import transformData from "../utils/transformData.js";
 import { Experiment, Variant } from "../models/experiment.js";
 
-// const baseUrl = "https://dog.ceo/api/breeds/list/all";
-
 const pgClient = new pg.Pool({ database: config.PG_DATABASE });
 pgClient.on("error", (err, client) => {
   console.error("Unexpected error on idle client", err);
@@ -16,8 +14,7 @@ pgClient.on("error", (err, client) => {
 async function createExperiment(req, res) {
   const { name, type_id, startDate, endDate, running, percentage } = req.body;
 
-  let newID;
-  let allData;
+  // let allData;
 
   const client = await pgClient.connect();
   try {
@@ -26,9 +23,11 @@ async function createExperiment(req, res) {
       [type_id, name, startDate, endDate, running, percentage]
     );
 
-    allData = await client.query("SELECT * FROM experiments WHERE name = $1", [
-      name,
-    ]);
+    let allData = await client.query(
+      "SELECT * FROM experiments WHERE name = $1",
+      [name]
+    );
+
     allData = allData.rows[0];
     let newExperiment = new Experiment(allData);
     console.log("New experiment passed back", newExperiment);
@@ -41,32 +40,70 @@ async function createExperiment(req, res) {
   }
 }
 
-// const uniquePath = generateUniquePath();
-// const customPath = req.body.endpoint || uniquePath;
-// console.log(customPath);
+async function updateExperiment(req, res) {}
 
-// const client = await pgClient.connect();
-// try {
-//   const response = await client.query(
-//     "INSERT INTO endpoints (path, binId) VALUES ($1, 1)",
-//     [customPath]
-//   );
-//   res.status(200).json(`Unique path: ${customPath} added`);
-// } catch (err) {
-//   res.status(403).json("Error in creating the endpoint on postgres");
-//   console.log(err.stack);
-// } finally {
-//   client.release();
-// }
+async function createVariants(req, res) {
+  try {
+    let variantArr = req.body.variants;
 
-// async function get_breed_list(req, res) {
-//   try {
-//     let data = await getBreedData(baseUrl);
-//     let result = transformData(data);
-//     res.status(200).send(result);
-//   } catch (error) {
-//     res.status(504).send(error);
-//   }
-// }
+    let addedVariants = [];
+    for (let i = 0; i < variantArr.length; i++) {
+      let variant = variantArr[i];
+      let newVar = await createVariant(variant, req, res);
+      addedVariants.push(newVar);
+      // push to array
+      // return new variant array
+    }
+    res.status(200).json({ variants: addedVariants, text: "Made it" });
+  } catch (error) {
+    res.status(403).json("Error in creating the variants in postgres");
+    console.log(error.stack);
+  } finally {
+    //client.release();
+  }
+}
+/*
+{
+  "varients": [
+    {
+      "experiment_id" : 16,
+      "value": "blue",
+      "is_control": true,
+      "weight": 0.5
+    },
+    {
+      "experiment_id" : 16,
+      "value": "red",
+      "is_control": false,
+      "weight": 0.5
+    }
+  ]
+}
+*/
 
-export { createExperiment };
+async function createVariant(ob, req, res) {
+  const client = await pgClient.connect();
+  try {
+    return {};
+    // const response = await client.query(
+    //   "INSERT INTO experiments (type_id, name, start_date, end_date, is_running, user_percentage) VALUES ($1, $2, $3, $4, $5, $6)",
+    //   [type_id, name, startDate, endDate, running, percentage]
+    // );
+    // allData = await client.query("SELECT * FROM experiments WHERE name = $1", [
+    //   name,
+    // ]);
+    // allData = allData.rows[0];
+    // let newExperiment = new Experiment(allData);
+    // console.log("New experiment passed back", newExperiment);
+    // res.status(200).json(newExperiment);
+  } catch (error) {
+    res.status(403).json("Error in updating the experiment in postgres");
+    console.log(error.stack);
+  } finally {
+    //client.release();
+  }
+}
+
+function updateVariants() {}
+
+export { createExperiment, updateExperiment, createVariants, updateVariants };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,18 @@
 import express from "express";
-import { createExperiment } from "../controllers/controller.js";
+import {
+  createExperiment,
+  updateExperiment,
+  createVariants,
+  updateVariants,
+} from "../controllers/controller.js";
 const router = express.Router();
 
 router.post("/api/experiment", createExperiment);
+
+router.put("/api/experiment/:id", updateExperiment);
+
+router.post("/api/experiment/:id/variants", createVariants);
+
+router.put("/api/experiment/:id/variants", updateVariants);
 
 export default router;
