4ce0e71e5c169ee7b899701f2a05e33d1ab36f0e
Sarah Bunker <ChemEnCoder@gmail.com>
2023-03-03 13:51:41 -0700

    adding testing for creating variants

--- src/routes/router.test.js
+++ src/routes/router.test.js
@@ -49,9 +49,52 @@ describe("Experiments API", () => {
 });
 
 describe("Variants API", () => {
-  let newVariants = []
+  let newVariants = {
+    "variants": [
+      {
+        "experiment_id" : 5,
+        "value": "red",
+      },
+      {
+        "experiment_id" : 5,
+        "value": "red"
+      }
+    ]
+  }
+
+  let newExperiment = {
+    "type_id": 3,
+    "name": "test_experiment",
+    "start_date": "2/23/23",
+    "end_date": "2/23/24",
+    "is_running": true,
+    "user_percentage": 0.5
+  }
 
   test( "testing create variant" , async () => {
+    let response = await supertest(app).post("/api/experiment").send(newExperiment);
+    let expId = response.body.id
 
+    response = await supertest(app).post(`/api/experiment/${expId}/variants`).send(newVariants);
+    expect(response.status).toEqual(403)
+    expect(response.body).toEqual("Error in creating the variants in postgres")
+
+    newVariants.variants.forEach(variant => variant.experiment_id = expId)
+    newVariants.variants[0].weight = 0.4
+    newVariants.variants[1].weight = 0.4
+
+
+    response = await supertest(app).post(`/api/experiment/${expId}/variants`).send(newVariants);
+    expect(response.status).toEqual(403)
+    expect(response.body).toEqual("Error in creating the variants in postgres")
+
+    newVariants.variants[0].weight = 0.5
+    newVariants.variants[1].weight = 0.5
+
+    response = await supertest(app).post(`/api/experiment/${expId}/variants`).send(newVariants);
+    expect(response.status).toEqual(200)
+    expect(Array.isArray(response.body.variants)).toEqual(true)
+
+    await supertest(app).delete(`/api/experiment/${expId}`)
   })
 })
