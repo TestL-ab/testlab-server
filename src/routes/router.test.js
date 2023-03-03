9619d3e9aafdfd0e054264d7fbb7bcf9a5448a71
Sarah Bunker <ChemEnCoder@gmail.com>
2023-03-03 11:48:52 -0700

    converting tests fully to use async await syntax

--- src/routes/router.test.js
+++ src/routes/router.test.js
@@ -1,22 +1,13 @@
-import { response } from "express";
 import supertest from "supertest";
-import request from "supertest";
 import app from "../app.js"
-const baseURL = "http://localhost:3000/api"
 
 describe("GET /experiment", () => {
 
   test( "testing GET /experiment", async () => {
 
-    await supertest(app).get("/api/experiment")
-      .expect(200)
-      .then( response => {
-        expect(Array.isArray(response.body)).toBeTruthy();
-        expect(response.body.length >= 1).toBe(true);
-      })
+    let response = await supertest(app).get("/api/experiment");
+    expect(response.status).toEqual(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body.length >= 1).toBe(true);
   })
-
-  // afterAll(async () => {
-  //   await new Promise(resolve => setTimeout(() => resolve(), 4000)); // avoid jest open handle error
-  // });
 });
