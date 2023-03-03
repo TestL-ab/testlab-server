3e4af285d7dce62f61d163536bba733275fd2b85
Sarah Bunker <ChemEnCoder@gmail.com>
2023-03-03 13:01:36 -0700

    adding some tests for when create experiment fails

--- src/routes/router.test.js
+++ src/routes/router.test.js
@@ -1,52 +1,38 @@
 import supertest from "supertest";
 import app from "../app.js"
 
-describe("GET /experiment", () => {
+describe("Experiments API", () => {
+  let newExperiment = {
+    "type_id": 3,
+    "name": "test_experiment",
+    "start_date": "2/23/23",
+    "end_date": "2/23/24",
+    "is_running": true,
+    "user_percentage": 0.5
+  }
 
-  test( "testing GET /experiment", async () => {
+  test( "testing get all experiment", async () => {
 
     let response = await supertest(app).get("/api/experiment");
     expect(response.status).toEqual(200);
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body.length >= 1).toBe(true);
   })
-});
 
-describe("post /experiment", () => {
-
-  test( "testing POST /experiment", async () => {
-
-    let newExperiment = {
-      "type_id": 3,
-      "name": "test_experiment",
-      "start_date": "2/23/23",
-      "end_date": "2/23/24",
-      "is_running": true,
-      "user_percentage": 0.5
-    }
-
+  test( "testing Create experiment", async () => {
     let response = await supertest(app).post("/api/experiment").send(newExperiment);
     expect(response.status).toEqual(200);
     expect(response.body.name).toEqual("test_experiment");
     let testID = response.body.id
 
+    response = await supertest(app).post("/api/experiment").send(newExperiment);
+    expect(response.status).toEqual(403);
+    expect(response.body).toEqual("Error in creating the experiment in postgres");
+
     await supertest(app).delete(`/api/experiment/${testID}`)
   })
-});
 
-describe("testing deleting experiments", () => {
-
-  test( "testing POST /experiment", async () => {
-
-    let newExperiment = {
-      "type_id": 3,
-      "name": "test_experiment",
-      "start_date": "2/23/23",
-      "end_date": "2/23/24",
-      "is_running": true,
-      "user_percentage": 0.5
-    }
-
+  test( "testing delete experiment", async () => {
     let response = await supertest(app).post("/api/experiment").send(newExperiment);
     let testID = response.body.id
 
@@ -61,3 +47,11 @@ describe("testing deleting experiments", () => {
     ).toBe(0)
   })
 });
+
+describe("Variants API", () => {
+  let newVariants = []
+
+  test( "testing create variant" , async () => {
+
+  })
+})
