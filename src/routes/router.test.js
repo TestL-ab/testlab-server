776a62fc15e7c045bf32915187860371de65f3cd
Sarah Bunker <ChemEnCoder@gmail.com>
2023-03-03 12:08:29 -0700

    adding test for creating an experiment

--- src/routes/router.test.js
+++ src/routes/router.test.js
@@ -11,3 +11,25 @@ describe("GET /experiment", () => {
     expect(response.body.length >= 1).toBe(true);
   })
 });
+
+describe("post /experiment", () => {
+
+  test( "testing POST /experiment", async () => {
+
+    let newExperiment = {
+      "type_id": 3,
+      "name": "test_experiment",
+      "start_date": "2/23/23",
+      "end_date": "2/23/24",
+      "is_running": true,
+      "user_percentage": 0.5
+    }
+
+    let response = await supertest(app).post("/api/experiment").send(newExperiment);
+    expect(response.status).toEqual(200);
+    expect(response.body.name).toEqual("test_experiment");
+    let testID = response.body.id
+
+    await supertest(app).delete(`/api/experiment/${testID}`)
+  })
+});
