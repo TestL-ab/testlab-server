c94da48af2802f21e7a4941fd3753ed730f2b21b
Alison Martinez <alison.a.martinez@outlook.com>
2023-03-02 20:23:42 -0500

    Docker containers working with psql

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,10 @@ import "dotenv/config.js";
 
 const PORT = process.env.PORT;
 const PG_DATABASE = process.env.PG_DATABASE;
+const DB_HOST = process.env.DB_HOST;
 
 export default {
   PORT,
   PG_DATABASE,
+  DB_HOST,
 };
